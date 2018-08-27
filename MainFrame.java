b8cb89d8fc75db76ea66e86d731b4d9bb0a70f15
Mathias Ronimus <mathiar225@gmail.com>
2018-08-27 20:33:14 +1200

    Add player character announcement

--- MainFrame.java
+++ MainFrame.java
@@ -23,53 +23,88 @@ public class MainFrame extends JFrame {
         this.game = game;
         
         getPlayerCount(() -> {
-        	        	
-        	//Set layout manager
-            this.getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
-            
-            //Create the menu bar
-            JMenuBar menuBar = new JMenuBar();
-            menuBar.add(new JMenu("Turn"));
-            menuBar.add(new JMenu("Game"));
-            menuBar.add(Box.createHorizontalGlue());
-            menuBar.setLayout(new FlowLayout(FlowLayout.LEFT));
-            
-            this.add(menuBar);
+        	
+        	tellPlayerCharacters(() -> {
+        		//Set layout manager
+                this.getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+                
+                //Create the menu bar
+                JMenuBar menuBar = new JMenuBar();
+                menuBar.add(new JMenu("Turn"));
+                menuBar.add(new JMenu("Game"));
+                menuBar.add(Box.createHorizontalGlue());
+                menuBar.setLayout(new FlowLayout(FlowLayout.LEFT));
+                
+                this.add(menuBar);
 
-            game.getBoard().draw();
-            
-            this.add(game.getBoard().panel);
-            
-            //Add the player's controls
-            JPanel playerControls = new JPanel();
-            playerControls.setLayout(new BoxLayout(playerControls, BoxLayout.Y_AXIS));
-            JLabel playerName = new JLabel("Player 1");
-            playerName.setForeground(Color.RED);
-            playerName.setFont(new Font("Arial", Font.PLAIN, 32));
-            playerControls.add(playerName);
-            
-            //Add the dice and cards
-            JPanel diceAndCards = new JPanel();
-            diceAndCards.setLayout(new BoxLayout(diceAndCards, BoxLayout.X_AXIS));
-            diceAndCards.add(new DiceJLabel(2));
-            diceAndCards.add(new DiceJLabel(6));
-            diceAndCards.add(Box.createHorizontalGlue());
-            JPanel cards = new JPanel();
-            cards.setLayout(new BoxLayout(cards, BoxLayout.Y_AXIS));
-            diceAndCards.add(cards);
-            cards.add(new CharacterJLabel(Game.CHARACTER_CARDS.get(0)));
-            cards.add(new WeaponJLabel(Game.WEAPON_CARDS.get(0)));
-            cards.add(new RoomJLabel(Game.ROOM_CARDS.get(0)));
-            
-            playerControls.add(diceAndCards);
-            
-            this.add(playerControls);
-        	setSize(1024, 1024);
-    	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    	    setVisible(true);
+                game.getBoard().draw();
+                
+                this.add(game.getBoard().panel);
+                
+                //Add the player's controls
+                JPanel playerControls = new JPanel();
+                playerControls.setLayout(new BoxLayout(playerControls, BoxLayout.Y_AXIS));
+                JLabel playerName = new JLabel("Player 1");
+                playerName.setForeground(Color.RED);
+                playerName.setFont(new Font("Arial", Font.PLAIN, 32));
+                playerControls.add(playerName);
+                
+                //Add the dice and cards
+                JPanel diceAndCards = new JPanel();
+                diceAndCards.setLayout(new BoxLayout(diceAndCards, BoxLayout.X_AXIS));
+                diceAndCards.add(new DiceJLabel(2));
+                diceAndCards.add(new DiceJLabel(6));
+                diceAndCards.add(Box.createHorizontalGlue());
+                JPanel cards = new JPanel();
+                cards.setLayout(new BoxLayout(cards, BoxLayout.Y_AXIS));
+                diceAndCards.add(cards);
+                cards.add(new CharacterJLabel(Game.CHARACTER_CARDS.get(0)));
+                cards.add(new WeaponJLabel(Game.WEAPON_CARDS.get(0)));
+                cards.add(new RoomJLabel(Game.ROOM_CARDS.get(0)));
+                
+                playerControls.add(diceAndCards);
+                
+                this.add(playerControls);
+            	setSize(1024, 1024);
+        	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        	    setVisible(true);
+        	});
         });
+    }
+    
+    /**
+     * Opens a dialog telling each player which character
+     * they are.
+     * 
+     * @param	next	The function to be run once OK is pressed.*/
+    private void tellPlayerCharacters(final Runnable next) {
+    	
+    	String text = "<html>";
+    	int i = 1;
+    	for (Player player : game.getPlayers()) {
+    		text += "Player " + i + ", you are " + player.getCharacter().getName() + "!<br>";
+    	}
+    	
+    	JPanel dialogPanel = new JPanel();
+        dialogPanel.setLayout(new FlowLayout());
+        JLabel textLabel = new JLabel(text);
+        JButton OKButton = new JButton("OK");
+        dialogPanel.add(textLabel);
+        dialogPanel.add(OKButton);
+        JDialog dialog = new JDialog(SwingUtilities.windowForComponent(this));
+        dialog.add(dialogPanel);
+        dialog.setSize(300, 200);
+        dialog.setVisible(true);
         
-        
+        OKButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				dialog.dispose();
+				next.run();
+			}
+        	
+        });
     }
     
     /**
@@ -116,6 +151,7 @@ public class MainFrame extends JFrame {
 				//Successful
 				game.assignCharacters(entered);
 				
+				dialog.dispose();
 				next.run();
 			}
  
